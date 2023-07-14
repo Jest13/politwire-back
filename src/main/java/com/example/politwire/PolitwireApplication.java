2d32671844226b74632a684065ac5d8f56189f4c
Jest13 <camil.hirane@ynov.com>
2023-07-14 17:49:35 +0200

    Initial commit

---
+++ src/main/java/com/example/politwire/PolitwireApplication.java
@@ -0,0 +1,13 @@
+package com.example.politwire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolitwireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolitwireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/politwire/PolitwireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.politwire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolitwireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
